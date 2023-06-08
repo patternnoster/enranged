66551471017d8c4d115bc7f84d595545a751659a
patternnoster <retsonnrettap@gmail.com>
2023-06-08 19:58:38 +0300

    Splicing templated test base

--- test/splicing_tests.cpp
+++ test/splicing_tests.cpp
@@ -1,5 +1,25 @@
+#include <forward_list>
 #include <gtest/gtest.h>
+#include <list>
 
+#include "enranged/limits.hpp"
 #include "enranged/splicing.hpp"
 
 using namespace enranged;
+
+template <typename T>
+class SplicingTests: public ::testing::Test {};
+
+using Spliceable = ::testing::Types<std::list<int>, std::forward_list<int>>;
+TYPED_TEST_SUITE(SplicingTests, Spliceable);
+
+TYPED_TEST(SplicingTests, concepts) {
+  EXPECT_TRUE((spliceable_range<TypeParam>));
+
+  TypeParam range{};
+  const auto bb = before_begin(range);
+
+  EXPECT_TRUE((std::sentinel_for<std::decay_t<decltype(bb)>,
+                                 ranges::iterator_t<TypeParam>>));
+  ASSERT_NE(bb, range.end());
+}
