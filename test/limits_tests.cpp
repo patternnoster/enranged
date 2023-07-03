23cbdd9e086a67138bad903d66e2d9aad2e4561c
patternnoster <retsonnrettap@gmail.com>
2023-07-03 21:46:02 +0300

    Separate limits tests & last() test

---
+++ test/limits_tests.cpp
@@ -0,0 +1,62 @@
+#include <deque>
+#include <forward_list>
+#include <gtest/gtest.h>
+#include <list>
+#include <vector>
+
+#include "enranged/limits.hpp"
+
+using namespace enranged;
+
+template <typename T>
+class LimitsTests: public ::testing::Test {};
+
+using Ranges = ::testing::Types<std::vector<int>, std::deque<int>,
+                                std::list<int>, std::forward_list<int>>;
+TYPED_TEST_SUITE(LimitsTests, Ranges);
+
+void push_front(auto& cont, const auto elt) { cont.push_front(elt); }
+void push_front(std::vector<int>& cont, const int elt) {
+  cont.insert(cont.begin(), elt);
+}
+
+TYPED_TEST(LimitsTests, before_begin) {
+  TypeParam range{};
+  const auto bb = before_begin(range);
+
+  EXPECT_TRUE((std::sentinel_for<std::decay_t<decltype(bb)>,
+                                 ranges::iterator_t<TypeParam>>));
+  ASSERT_NE(bb, range.end());
+
+  EXPECT_TRUE((left_limit_of<std::decay_t<decltype(bb)>, TypeParam>));
+  EXPECT_TRUE((left_limit_of<std::decay_t<decltype(ranges::begin(range))>,
+                                          TypeParam>));
+
+  EXPECT_TRUE((right_limit_of<std::decay_t<decltype(ranges::begin(range))>,
+                                          TypeParam>));
+  EXPECT_TRUE((right_limit_of<std::decay_t<decltype(ranges::end(range))>,
+                                          TypeParam>));
+
+  EXPECT_EQ(after(range, bb), ranges::begin(range));
+
+  push_front(range, 42);
+  EXPECT_EQ(after(range, bb), ranges::begin(range));
+}
+
+template <typename T>
+class CorangeLimitsTests: public ::testing::Test {};
+
+using Coranges = ::testing::Types<std::vector<int>, std::deque<int>,
+                                  std::list<int>>;
+TYPED_TEST_SUITE(CorangeLimitsTests, Coranges);
+
+TYPED_TEST(CorangeLimitsTests, last) {
+  TypeParam range{};
+  EXPECT_TRUE((corange<TypeParam>));
+
+  push_front(range, 42);
+  push_front(range, 17);
+
+  EXPECT_EQ(*last(range), 42);
+  EXPECT_EQ(after(range, last(range)), ranges::end(range));
+}
--- test/splicing_tests.cpp
+++ test/splicing_tests.cpp
@@ -2,7 +2,6 @@
 #include <gtest/gtest.h>
 #include <list>
 
-#include "enranged/limits.hpp"
 #include "enranged/splicing.hpp"
 
 using namespace enranged;
@@ -16,25 +15,4 @@ TYPED_TEST_SUITE(SplicingTests, Spliceable);
 TYPED_TEST(SplicingTests, concepts) {
   EXPECT_TRUE((spliceable_range<TypeParam>));
   EXPECT_TRUE((spliceable_with_range<TypeParam, TypeParam>));
-
-  TypeParam range{};
-  const auto bb = before_begin(range);
-
-  EXPECT_TRUE((std::sentinel_for<std::decay_t<decltype(bb)>,
-                                 ranges::iterator_t<TypeParam>>));
-  ASSERT_NE(bb, range.end());
-
-  EXPECT_TRUE((left_limit_of<std::decay_t<decltype(bb)>, TypeParam>));
-  EXPECT_TRUE((left_limit_of<std::decay_t<decltype(ranges::begin(range))>,
-                                          TypeParam>));
-
-  EXPECT_TRUE((right_limit_of<std::decay_t<decltype(ranges::begin(range))>,
-                                          TypeParam>));
-  EXPECT_TRUE((right_limit_of<std::decay_t<decltype(ranges::end(range))>,
-                                          TypeParam>));
-
-  EXPECT_EQ(after(range, bb), ranges::begin(range));
-
-  range.push_front(42);
-  EXPECT_EQ(after(range, bb), ranges::begin(range));
 }
