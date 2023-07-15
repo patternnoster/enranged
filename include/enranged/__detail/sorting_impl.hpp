b827e677f072b8901438bca7f9de4a7de72e07be
patternnoster <retsonnrettap@gmail.com>
2023-07-16 01:58:12 +0300

    Implementation of coinplace_merge_splice()

--- include/enranged/__detail/sorting_impl.hpp
+++ include/enranged/__detail/sorting_impl.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <concepts>
 #include <functional>
+#include <iterator>
 #include <type_traits>
 #include <utility>
 
@@ -36,7 +37,60 @@ template <spliceable_range R, left_limit_of<R> L, typename Comp>
 constexpr ranges::borrowed_iterator_t<R> coinplace_merge_splice
   (R&& range, const L left, const ranges::iterator_t<R> middle,
    const ranges::iterator_t<R> last, const Comp comp) {
-  return last;
+  // We assume left != last, since middle must be in the corange
+  if (middle == last) [[unlikely]] return last;
+
+  auto rhs = ranges::next(middle);  // First element on the right
+  const auto end = ranges::next(last);
+
+  // Invariant #1: rhs != end (forward implies equality preserving)
+
+  // Some optimistic thinking: what if everything is already sorted?
+  if (!comp(*rhs, *middle)) return last;
+
+  // Our optimistic thinking has given us:
+  // Invariant #2: *rhs < *middle
+
+  auto lhs = after(range, left); // First unsorted element on the left
+
+  // We need a separate splice in case *rhs compares less than the
+  // front element
+  if (comp(*rhs, *lhs)) { /* <= (lhs == middle) */
+    // We need to put some part of the right side front, figure out
+    // how big of a part that is
+    ranges::iterator_t<R> rhs_next = ranges::next(rhs);
+    for (; rhs_next != end && comp(*rhs_next, *lhs); rhs = rhs_next++);
+
+    cosplice(range, left, middle, rhs);
+
+    if (rhs_next == end) return middle;
+    if (lhs == middle || !comp(*rhs_next, *middle)) return last;
+
+    rhs = rhs_next;
+  }
+
+  // Invariant #3: lhs != middle
+  // Invariant #4: *lhs <= *rhs
+
+  for (;;) {
+    // Find the first left-hand side element that is greater than
+    // *rhs. Because of invariant #2, such element always exists
+    ranges::iterator_t<R> lhs_next = ranges::next(lhs);
+    for (; !comp(*rhs, *lhs_next); lhs = lhs_next++);
+
+    // Now *lhs <= *rhs < *lhs_next. Find out, how big of a part
+    // following rhs we can splice in-between them
+    ranges::iterator_t<R> rhs_next = ranges::next(rhs);
+    for (; rhs_next != end && comp(*rhs_next, *lhs_next); rhs = rhs_next++);
+
+    cosplice(range, lhs, middle, rhs);
+
+    if (rhs_next == end) return middle;
+    if (lhs_next == middle || !comp(*rhs_next, *middle)) return last;
+
+    lhs = lhs_next;
+    rhs = rhs_next;
+  }
 }
 
 } // namespace enranged::__detail
