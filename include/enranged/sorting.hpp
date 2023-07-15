150553cd970783ce0b81d3b7da076fc77058cc6a
patternnoster <retsonnrettap@gmail.com>
2023-07-15 20:30:02 +0300

    The coinplace_merge_splice() declaration & description

--- include/enranged/sorting.hpp
+++ include/enranged/sorting.hpp
@@ -47,4 +47,37 @@ concept splice_sortable_range = spliceable_range<R>
                                      std::projected<ranges::iterator_t<R>,
                                                     Proj>>;
 
+/**
+ * @brief  Given a subrange (left, right] of a spliceable range and an
+ *         iterator mid from that subrange, assumes the subranges
+ *         (left, mid] and (mid, right] are sorted, performs a stable
+ *         inplace splice-based merge into one sorted subrange (left,
+ *         result], and returns result
+ *
+ * This version is dual to the regular std::inplace_merge(), which,
+ * given iterators a, b and c, performs the merging of ranges [a, b)
+ * and [b, c) instead of coranges (a, b] and (b, c] like
+ * here. Furthermore, this version returns the last element of the
+ * resulting range and not the unchanged upper bound (which makes the
+ * returned value meaningful)
+ *
+ * @tparam Comp must be a strict weak order (see above)
+ * @param  left must be a valid left limit of the given range (i.e., a
+ *         front sentinel or a dereferenceable iterator)
+ * @param  mid must be a dereferenceable iterator in (left, right]
+ * @param  right must be a dereferenceable iterator of the given range,
+ *         such that (left, right] is a valid corange (in particular,
+ *         left != right)
+ * @return The iterator to the last element of the sorted subrange
+ * @note   The behaviour is undefined if either of (left, mid] or
+ *         (mid, right] are not sorted
+ **/
+template <spliceable_range R, left_limit_of<R> L,
+          typename Comp = ranges::less, typename Proj = std::identity>
+  requires(splice_sortable_range<R, Comp, Proj>)
+constexpr ranges::borrowed_iterator_t<R> coinplace_merge_splice
+  (R&& range, const L left, const ranges::iterator_t<R> mid,
+   const ranges::iterator_t<R> right,
+   const Comp comp = {}, const Proj proj = {});
+
 } // namespace enranged
