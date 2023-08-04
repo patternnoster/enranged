1272df5fed6adb2a37310de6b36349c17e31fa1a
patternnoster <retsonnrettap@gmail.com>
2023-08-05 01:49:59 +0300

    Iterator for flat_list

--- include/enranged/__detail/flat_list.hpp
+++ include/enranged/__detail/flat_list.hpp
@@ -47,6 +47,79 @@ public:
     clear();
   }
 
+  class iterator {
+  public:
+    using value_type = T;
+    using difference_type = std::ptrdiff_t;
+
+    iterator() noexcept = default;
+    bool operator==(const iterator& other) const noexcept {
+      return pos_ == other.pos_;  // Comparison between different
+                                  // containers is UB anyway
+    }
+
+    T* operator->() const noexcept {
+      return (reinterpret_cast<T*>(list_->data_) + pos_);
+    }
+
+    T& operator*() const noexcept {
+      return *this->operator->();
+    }
+
+    iterator& operator++() noexcept {
+      pos_ = list_->links_[pos_ + 1];
+      return *this;
+    }
+
+    iterator operator++(int) noexcept {
+      iterator result{*this};
+      ++*this;
+      return result;
+    }
+
+  private:
+    iterator(flat_list* const list, const size_t pos) noexcept:
+      list_(list), pos_(pos) {}
+
+    flat_list* list_;
+    size_t pos_;
+
+    friend class flat_list;
+  };
+
+  /**
+   * @brief  Constructs an element after the given iterator using the
+   *         provided arguments. No iterators are invalidated. If the
+   *         list already has _max_size elements or (it) is not
+   *         before_begin() or dereferenceable, the behaviour is
+   *         undefined
+   * @return An iterator to the newly constructed element
+   **/
+  template <typename... Args>
+  iterator emplace_after(const iterator& it, Args&&... args)
+    noexcept(std::is_nothrow_constructible_v<T, Args...>) {
+    new (static_cast<void*>(data_ + sizeof(T) * size_))
+      T{std::forward<Args>(args)...};
+
+    auto& it_link = links_[it.pos_ + 1];
+    links_[size_ + 1] = it_link;
+    it_link = pos_t(size_);
+
+    return { this, size_++ };
+  }
+
+  iterator before_begin() noexcept {
+    return { this, size_t(-1) };
+  }
+
+  iterator begin() noexcept {
+    return { this, *links_ };
+  }
+
+  iterator end() noexcept {
+    return { this, _max_size };
+  }
+
   size_t size() const noexcept {
     return size_;
   }
@@ -65,6 +138,7 @@ private:
                                // (i-1)-th element (first if i == 0)
 
   alignas(T) byte data_[sizeof(T) * _max_size];
+  friend class iterator;
 };
 
 } // namespace enranged::__detail
